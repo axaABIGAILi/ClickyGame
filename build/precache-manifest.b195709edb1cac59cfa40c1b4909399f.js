b65abd9253be85471686569bb3169fbf0abc899f
Abi D <axa.abigail.i@gmail.com>
2019-09-06 21:18:37 -0500

    created build file for deployment

---
+++ build/precache-manifest.b195709edb1cac59cfa40c1b4909399f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e391fd8f2b4d8796df0c3aed63bcdfc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca289e83cdddb64c155d",
+    "url": "/static/css/main.09976b93.chunk.css"
+  },
+  {
+    "revision": "ff10930311ee016e5d96",
+    "url": "/static/js/2.6704351c.chunk.js"
+  },
+  {
+    "revision": "ca289e83cdddb64c155d",
+    "url": "/static/js/main.d8476ece.chunk.js"
+  },
+  {
+    "revision": "0ebfaf63642c8e34abd7",
+    "url": "/static/js/runtime~main.b027e18f.js"
+  },
+  {
+    "revision": "f0790b04a0392f7ff21b8624c7e78a9c",
+    "url": "/static/media/SmashBall.f0790b04.svg"
+  }
+]);
